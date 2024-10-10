414a83e9b52ff8154dbc97963c2345522a754b24
oggunderscore <oggunderscore@gmail.com>
2024-10-09 20:30:38 -0700

    Added NavBar, Loading Page, Loading Bar Started templating pages and component framework

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            TEMPORARY NAVBAR
+          </Typography>
+          <Button color="inherit" onClick={() => navigate("/")}>
+            Home
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/loadingpage")}>
+            Loading Page
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/recipegenerator")}>
+            Recipe Generator
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
