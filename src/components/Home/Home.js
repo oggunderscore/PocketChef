e5d7f37c6bf7db7cb74188fb0ea0aba3dea096b3
oggunderscore <oggunderscore@gmail.com>
2024-10-16 19:26:50 -0700

    Fixed ESLint configuration

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { Slider } from '@mui/material';
-import './Home.css';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Slider } from "@mui/material";
+import "./Home.css";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 function Home() {
-
   const handleAddIngredient = () => {
     console.log("Adding ingredient...");
-  }
+  };
 
   return (
     <div className="app-container">
       <div className="app-bar">
         <h1 className="app-title">PocketChef</h1>
       </div>
-
-      
 
       <h2 className="welcome">Welcome to PocketChef!</h2>
       <h3 className="pref">Let's set your preferences.</h3>
@@ -27,9 +24,9 @@ function Home() {
           defaultValue={0.5}
           step={0.1}
           marks={[
-            { value: 0, label: 'Low' },
+            { value: 0, label: "Low" },
             { value: 0.5 },
-            { value: 1, label: 'High' }
+            { value: 1, label: "High" },
           ]}
           min={0}
           max={1}
@@ -44,9 +41,9 @@ function Home() {
           defaultValue={0.5}
           step={0.1}
           marks={[
-            { value: 0, label: 'Low' },
+            { value: 0, label: "Low" },
             { value: 0.5 },
-            { value: 1, label: 'High' }
+            { value: 1, label: "High" },
           ]}
           min={0}
           max={1}
@@ -61,9 +58,9 @@ function Home() {
           defaultValue={0.5}
           step={0.1}
           marks={[
-            { value: 0, label: 'Low' },
+            { value: 0, label: "Low" },
             { value: 0.5 },
-            { value: 1, label: 'High' }
+            { value: 1, label: "High" },
           ]}
           min={0}
           max={1}
@@ -75,11 +72,12 @@ function Home() {
         <p className="ingredient-text">Ingredients:</p>
         <div className="add-ingredient" onClick={handleAddIngredient}>
           <div className="blue-box">
-            <div className="add-button">Click to Add <AddCircleIcon /></div>
+            <div className="add-button">
+              Click to Add <AddCircleIcon />
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 }
