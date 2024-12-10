59ca7faf83fc95ed541c3d723b58a6e5de2663b6
Kevin Nguyen <oggunderscore@gmail.com>
2024-12-09 16:45:31 -0800

    Oggunderscore/main updates (#25)
    * Temp
    
    * Small fix to the output of recipe page
    
    * temp upload

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,10 @@
-require('dotenv').config();  // Load .env file at runtime
-const functions = require('firebase-functions');
-const OpenAI = require('openai');
-
+require("dotenv").config(); // Load .env file at runtime
+const functions = require("firebase-functions");
+const OpenAI = require("openai");
 
 // Initialize OpenAI SDK using the new structure
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "OPENAIKEY",
 });
 
 exports.generateText = functions.https.onCall(async (request) => {
@@ -13,23 +12,26 @@ exports.generateText = functions.https.onCall(async (request) => {
     console.log("Received data:", request); // Log the incoming data
 
     // Checking that the prompt is provided and is a non-empty string
-    if (!(typeof request.data.prompt === "string") || request.data.prompt.length === 0) {
-      throw new functions.https.HttpsError("invalid-argument", "The function must be called with a valid 'prompt' argument. Data type is: ",(typeof request.data.prompt),"prompt length is: ".request.data.prompt.length);
-    }
-
-    // Checking that the user is authenticated
-    if (!request.auth) {
-      throw new functions.https.HttpsError("failed-precondition", "The function must be called while authenticated.");
+    if (
+      !(typeof request.data.prompt === "string") ||
+      request.data.prompt.length === 0
+    ) {
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "The function must be called with a valid 'prompt' argument. Data type is: ",
+        typeof request.data.prompt,
+        "prompt length is: ".request.data.prompt.length
+      );
     }
 
     const response = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: request.data.prompt }],
-      model: 'gpt-4',
+      messages: [{ role: "user", content: request.data.prompt }],
+      model: "gpt-4o",
     });
 
     return { response: response.choices[0].message.content };
   } catch (error) {
-    console.error('Error generating text:', error);
-    throw new functions.https.HttpsError('internal', error);
+    console.error("Error generating text:", error);
+    throw new functions.https.HttpsError("internal", error);
   }
 });
