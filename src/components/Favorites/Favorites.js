b8f681d601a58dee1f73faecfa9cddaf938fbfd8
oggunderscore <oggunderscore@gmail.com>
2024-12-17 13:56:05 -0800

    Started Implementing Favorites Page

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,251 +1,77 @@
 import React, { useState, useEffect } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import "./Favorites.css";
-import ArrowForwardIosRounded from "@mui/icons-material/ArrowForwardIosRounded";
-import MoreVertTwoToneIcon from "@mui/icons-material/MoreVertTwoTone";
-import { Menu, MenuItem } from "@mui/material";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import retrieveRecipe from "../Hooks/RetrieveRecipe";
+import "./Favorites.css";
 
 const Favorites = () => {
-  const [folders, setFolders] = useState([]);
-  const [expandedFolders, setExpandedFolders] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredRecipes, setFilteredRecipes] = useState([]);
-  const [newFolderName, setNewFolderName] = useState("");
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [selectedFolder, setSelectedFolder] = useState(null);
-  const [isSearchFocused, setIsSearchFocused] = useState(false);
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]); // List of fetched recipes
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const auth = getAuth();
+  const db = getFirestore();
 
   useEffect(() => {
-    const fetchFolders = async () => {
-      const data = [
-        {
-          id: "1",
-          name: "Vegetarian",
-          recipes: [
-            { id: "r1", name: "Potato Salad" },
-            { id: "r2", name: "Roasted Asparagus" },
-          ],
-        },
-        {
-          id: "2",
-          name: "Keto",
-          recipes: [
-            { id: "r3", name: "Keto Pancakes" },
-            { id: "r4", name: "Grilled Salmon" },
-            { id: "r5", name: "Avocado Salad" },
-          ],
-        },
-        {
-          id: "3",
-          name: "Vegan",
-          recipes: [
-            { id: "r6", name: "Vegan Burger" },
-            { id: "r7", name: "Quinoa Salad" },
-            { id: "r8", name: "Vegan Brownies" },
-          ],
-        },
-      ];
-      setFolders(data);
+    const fetchFavorites = async () => {
+      const user = auth.currentUser;
+      if (!user) {
+        setError("User not logged in.");
+        setLoading(false);
+        return;
+      }
+
+      try {
+        // Fetch the user's favorite_recipes array
+        const userRef = doc(db, "users", user.uid);
+        const userSnap = await getDoc(userRef);
+
+        if (userSnap.exists()) {
+          const { favorite_recipes } = userSnap.data();
+          if (favorite_recipes && favorite_recipes.length > 0) {
+            // Fetch each recipe data using the retrieveRecipe function
+            const recipesData = await Promise.all(
+              favorite_recipes.map((id) => retrieveRecipe(id))
+            );
+            setFavoriteRecipes(recipesData.filter((recipe) => recipe !== null));
+          }
+        } else {
+          setError("No favorite recipes found.");
+        }
+      } catch (err) {
+        console.error("Error fetching favorite recipes:", err);
+        setError("Failed to fetch favorite recipes.");
+      } finally {
+        setLoading(false);
+      }
     };
 
-    fetchFolders();
-  }, []);
-
-  useEffect(() => {
-    if (searchQuery) {
-      const allRecipes = folders.flatMap((folder) => folder.recipes);
-      const filtered = allRecipes.filter((recipe) =>
-        recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-      setFilteredRecipes(filtered);
-    } else {
-      setFilteredRecipes([]);
-    }
-  }, [searchQuery, folders]);
-
-  const toggleFolder = (folderId) => {
-    if (expandedFolders.includes(folderId)) {
-      setExpandedFolders(expandedFolders.filter((id) => id !== folderId));
-    } else {
-      setExpandedFolders([...expandedFolders, folderId]);
-    }
-  };
-
-  const createFolder = () => {
-    if (newFolderName.trim()) {
-      setFolders([
-        ...folders,
-        { id: Date.now().toString(), name: newFolderName, recipes: [] },
-      ]);
-      setNewFolderName("");
-    }
-  };
-
-  const handleMenuClick = (event, folderId) => {
-    setAnchorEl(event.currentTarget);
-    setSelectedFolder(folderId);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    setSelectedFolder(null);
-  };
-
-  const renameFolder = () => {
-    const newName = prompt(
-      "Enter new folder name:",
-      folders.find((folder) => folder.id === selectedFolder)?.name
-    );
-    if (newName) {
-      setFolders(
-        folders.map((folder) =>
-          folder.id === selectedFolder ? { ...folder, name: newName } : folder
-        )
-      );
-    }
-    handleMenuClose();
-  };
-
-  const deleteFolder = () => {
-    setFolders(folders.filter((folder) => folder.id !== selectedFolder));
-    handleMenuClose();
-  };
-
-  const onDragEnd = (result) => {
-    if (!result.destination) return;
-
-    const reorderedFolders = Array.from(folders);
-    const [removed] = reorderedFolders.splice(result.source.index, 1);
-    reorderedFolders.splice(result.destination.index, 0, removed);
-
-    setFolders(reorderedFolders);
-  };
+    fetchFavorites();
+  }, [auth, db]);
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div className="favorites-container">
-        <h2 className="favorites-title">Favorites</h2>
-        <div
-          className="search-container"
-          onMouseDown={(e) => {
-            if (!e.target.closest(".search-dropdown")) {
-              setIsSearchFocused(false);
-            }
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Search Favorites"
-            className="search-input"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onFocus={() => setIsSearchFocused(true)}
-            onBlur={() => setIsSearchFocused(false)}
-          />
-          {filteredRecipes.length > 0 && isSearchFocused && (
-            <ul
-              className="search-dropdown"
-              onMouseDown={(e) => e.preventDefault()}
+    <div className="favorites-container">
+      <h2 className="favorites-title">Favorites</h2>
+      {loading ? (
+        <p>Loading your favorites...</p>
+      ) : error ? (
+        <p className="error-message">{error}</p>
+      ) : favoriteRecipes.length > 0 ? (
+        <ul className="favorites-list">
+          {favoriteRecipes.map((recipe) => (
+            <li
+              key={recipe.recipe_id}
+              className="favorite-item"
+              onClick={() => console.log("Recipe Details:", recipe)}
             >
-              {filteredRecipes.map((recipe) => (
-                <li
-                  key={recipe.id}
-                  className="search-item"
-                  onClick={() => retrieveRecipe(recipe.id)}
-                >
-                  {recipe.name}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className="create-folder-container">
-          <input
-            type="text"
-            placeholder="New Folder Name"
-            value={newFolderName}
-            onChange={(e) => setNewFolderName(e.target.value)}
-          />
-          <button onClick={createFolder}>Create Folder</button>
-        </div>
-        <Droppable droppableId="droppable-folders">
-          {(provided) => (
-            <ul
-              className="favorites-list"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              {folders.map((folder, index) => (
-                <Draggable
-                  key={folder.id}
-                  draggableId={folder.id}
-                  index={index}
-                >
-                  {(provided) => (
-                    <li
-                      className="favorites-folder"
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      onClick={() => toggleFolder(folder.id)}
-                    >
-                      <div className="folder-header">
-                        <MoreVertTwoToneIcon
-                          className="more-icon"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleMenuClick(e, folder.id);
-                          }}
-                        />
-                        <ArrowForwardIosRounded
-                          className={`folder-icon ${
-                            expandedFolders.includes(folder.id)
-                              ? "expanded"
-                              : ""
-                          }`}
-                        />
-                        <span className="folder-name">{folder.name}</span>
-                      </div>
-                      <div
-                        className={`folder-recipes ${
-                          expandedFolders.includes(folder.id) ? "expanded" : ""
-                        }`}
-                      >
-                        <ul className="favorites-recipe-list">
-                          {folder.recipes.map((recipe) => (
-                            <li
-                              key={recipe.id}
-                              className="favorites-recipe-item"
-                            >
-                              <button
-                                className="favorites-recipe-button"
-                                onClick={() => retrieveRecipe(recipe.id)}
-                              >
-                                {recipe.name}
-                              </button>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    </li>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem onClick={renameFolder}>Rename</MenuItem>
-          <MenuItem onClick={deleteFolder}>Delete</MenuItem>
-        </Menu>
-      </div>
-    </DragDropContext>
+              {recipe.recipe.recipe_name}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No favorite recipes found.</p>
+      )}
+    </div>
   );
 };
 
